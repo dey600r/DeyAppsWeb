b5476dc6b3e8406b5842078c72504bf7293dfaf9
dey125r <dey125r@gmail.com>
2023-05-30 18:50:35 +0200

    Test build redhat

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -26,7 +26,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: this.translator.instant('COMMON.home') + 'Test David build5',
+        label: this.translator.instant('COMMON.home') + 'Test David build6',
         icon: 'pi pi-fw pi-home',
         routerLink: [Constants.ROUTE_HOME]
       },

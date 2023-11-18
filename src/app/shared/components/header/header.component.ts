9fd7a06f8dcb03ce2a53bbe2bf3128ed4db320ee
dey125r <dey125r@gmail.com>
2023-11-18 17:24:28 +0100

    Add version to web

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Location } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -6,16 +6,23 @@ import { MenuItem } from 'primeng/api';
 import { Constants } from '@app/core/utils/constants';
 import { Router, Event } from '@angular/router';
 import cssVars from 'css-vars-ponyfill';
+import { Tooltip } from 'primeng/tooltip';
+import { environment } from '@environments/environment';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
 
+  @ViewChild(Tooltip) tooltip!: Tooltip;
+
   items: MenuItem[] = [];
   selectedItem: MenuItem;
+
+  versionApp: string = `v${environment.lastVersion} - ${environment.lastUpdate}`;
 
   constructor(private location: Location,
               private router: Router,
@@ -43,6 +50,10 @@ export class HeaderComponent implements OnInit {
         this.selectedItem = this.items[0];
       }
     });
+  }
+
+  showTooltip() {
+    this.tooltip.activate();
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+import packageInfo from '../../package.json';
+
 export const environment = {
   production: true,
+  lastVersion: packageInfo.version,
+  lastUpdate: packageInfo.dateVersion,
   pathTranslate: './assets/i18n/',
   pathIcons: './assets/images/icons',
   pathOthers: './assets/images/others',
